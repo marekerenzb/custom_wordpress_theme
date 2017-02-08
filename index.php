66dfbebbc52aaae9194d30d7fc49931535297252
Mark D <mark@qstrike.com>
2017-02-08 14:20:38 +0800

    code refactor

--- index.php
+++ index.php
@@ -15,10 +15,12 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
+
 		<main id="main" class="site-main" role="main">
 				
 
 		</main><!-- #main -->
+
 	</div><!-- #primary -->
 
 <?php
